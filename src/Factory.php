9d4abb22cdac8d96310a3f709d8e5b0cb8a4f1d7
liqunx <1037861925@qq.com>
2018-11-09 17:50:23 +0800

    增加工厂类

---
+++ src/Factory.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * This file is part of the overtrue/wechat.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Liqunx\Baidu;
+
+/**
+ * Class Factory.
+ *
+ * @method static \Liqunx\Baidu\Ai\Application            aip(array $config)
+ */
+class Factory
+{
+    /**
+     * @param string $name
+     * @param array  $config
+     *
+     * @return \Liqunx\Baidu\Kernel\ServiceContainer
+     */
+    public static function make($name, array $config)
+    {
+        $namespace = self::studly($name);
+        $application = "\\Liqunx\\Baidu\\{$namespace}\\Application";
+
+        return new $application($config);
+    }
+
+    /**
+     * Dynamically pass methods to the application.
+     *
+     * @param string $name
+     * @param array  $arguments
+     *
+     * @return mixed
+     */
+    public static function __callStatic($name, $arguments)
+    {
+        return self::make($name, ...$arguments);
+    }
+
+    /**
+     * Convert a value to studly caps case.
+     *
+     * @param string $name
+     * @return string
+     */
+    private static function studly($name='aip') :string
+    {
+        if ($name === 'aip') {
+            $name = 'ai';
+        }
+        return ucwords($name);
+    }
+}
